bd42b271271e84cc384fc86bae01b1b5e2c2cef6
Anjum Ansari <anjum2000ansari@gmail.com>
2025-10-03 15:09:50 +0530

    function with conditions

---
+++ functions.js
@@ -0,0 +1,47 @@
+
+//task - make a calculator 
+
+function calculateGrade(marks) {
+
+    switch (true) {
+        case marks > 90:
+            return "A";
+
+        case marks > 80:
+            return "B";
+
+        case marks > 70:
+            return "C";
+
+        case marks < 60:
+            return "D";
+
+        default:
+            return 'F';
+
+    }
+
+}
+
+console.log(calculateGrade(77));
+
+
+//with if- else using
+
+function gradeCalculator (grade){
+
+    if(grade > 90){
+        return "A";
+    }else if(grade <90 && grade >80){
+        return "B";
+    }else if(grade <80 && grade >60){
+        return "C";
+    }else if(grade <60 && grade >40){
+        return "D";
+    }else{
+        return "F";
+    }
+}
+
+console.log(gradeCalculator(53));
+
